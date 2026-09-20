1edbbce3e4b8360deefbc3a204bcb45c5b321d2f
randireswaraaa <randireswara089@gmail.com>
2026-09-20 14:03:00 +0700

    feat: embed Google Maps at address section in contact page and footer

--- src/config/site.ts
+++ src/config/site.ts
@@ -14,6 +14,8 @@ export interface SiteConfig {
     country: string;
     region: string;
   };
+  googleMapsUrl?: string;
+  googleMapsEmbedUrl?: string;
   businessHours: string;
   description: string;
   defaultOgImage: string;
@@ -29,7 +31,7 @@ export interface SiteConfig {
 }
 
 const siteUrl = import.meta.env.PUBLIC_SITE_URL || "https://sewamobilmatic.id";
-const waNumber = import.meta.env.PUBLIC_WHATSAPP_NUMBER || "+6287864264262";
+const waNumber = "+6287864264262";
 
 export const siteConfig: SiteConfig = {
   name: "Sewa Mobil Matic",
@@ -47,6 +49,9 @@ export const siteConfig: SiteConfig = {
     country: "ID",
     region: "Jabodetabek",
   },
+  googleMapsUrl: "https://maps.app.goo.gl/AjTdVSqWTYwxwykN7?g_st=ac",
+  googleMapsEmbedUrl:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8!2d106.685!3d-6.307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fad021c8da79%3A0x1fc3ae6d8086ffdd!2sPM4Q%2B44H%2C%20Lengkong%20Gudang%20Tim.%2C%20Kec.%20Serpong%2C%20Kota%20Tangerang%20Selatan%2C%20Banten%2015310!5e0!3m2!1sid!2sid!4v1700000000000!5m2!1sid!2sid",
   businessHours: "Mo-Su 07:00-22:00",
   description:
     "Layanan rental mobil matic terpercaya untuk kebutuhan harian dan bulanan di area BSD, Tangerang Selatan, Alam Sutera, Jakarta, dan sekitarnya.",
